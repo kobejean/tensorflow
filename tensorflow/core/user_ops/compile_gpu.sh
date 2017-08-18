728b255d68d31386995dacf02134a834a88a11d1
Jean Flaherty <kobejean@me.com>
2017-08-18 14:54:48 -0600

    compile fix

--- tensorflow/core/user_ops/compile_gpu.sh
+++ tensorflow/core/user_ops/compile_gpu.sh
@@ -3,5 +3,5 @@ nvcc -std=c++11 -c -o roll_gpu.cu.o roll_gpu.cu.cc \
 -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 g++ -std=c++11 -shared -o roll.so roll.cc \
-roll_gpu.cu.o -I $TF_INC -fPIC -lcudart
+roll_gpu.cu.o -I $TF_INC -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 # -lcudart
 python3 roll_test.py
