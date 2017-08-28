6374791b2d94636f2a270d71a9cacfe704dbaee2
Jean Flaherty <kobejean@me.com>
2017-08-28 14:53:53 -0600

    GPU bug fix

--- tensorflow/core/kernels/roll_op_gpu.cu.cc
+++ tensorflow/core/kernels/roll_op_gpu.cu.cc
@@ -92,9 +92,10 @@ struct RollFunctor<GPUDevice, T> {
 // Definition of the GPU implementations declared in roll_op.h.
 #define DEFINE_GPU_SPECS(T)                  \
   template struct RollFunctor<GPUDevice, T>; \
-  TF_CALL_GPU_NUMBER_TYPES(DEFINE_GPU_SPECS);
-
 #undef DEFINE_GPU_SPECS
+
+TF_CALL_GPU_NUMBER_TYPES(DEFINE_GPU_SPECS);
+
 
 }  // namespace functor
 }  // namespace tensorflow
